3e5ce1b5d659e1f27e998bb2bec7baf65258192e
pld360 <134562508+pld360@users.noreply.github.com>
2025-03-19 13:15:27 +0000

    gotowe zad

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,12 @@
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+        // Deklaracja zmiennej i przypisanie wartości
+        int zmienna = 17;
 
+        // Wypisanie wartości zmiennej na ekranie
+        System.out.println("Wartość zmiennej: " + zmienna);
+  }
   // @Test
   // void addition() {
   //     assertEquals(2, 1 + 1);

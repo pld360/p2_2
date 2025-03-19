692d3911d9c6b821880a58197423edb72bde701a
pld360 <134562508+pld360@users.noreply.github.com>
2025-03-19 13:37:36 +0000

    od 0 do 100 z użyciem pętli

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,12 @@
 // import org.junit.jupiter.api.Test;
 
 public class Main {
-  public static void main(String[] args) {
-        // Deklaracja zmiennej i przypisanie wartości
-        int zmienna = 17;
-
-        // Wypisanie wartości zmiennej na ekranie
-        System.out.println("Wartość zmiennej: " + zmienna);
-  }
+    public static void main(String[] args) {
+        // Pętla, która wypisuje liczby od 0 do 100
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
   // @Test
   // void addition() {
   //     assertEquals(2, 1 + 1);
